1de96d42afc103f2ba61de483da1dbb196834036
Jorge Sanguinetti <mrsanguinetti93@gmail.com>
2022-07-24 15:03:42 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ContadorModule } from './contador/contador/contador.module';
+import { HeroesModule } from './heroes/heroes.module';
 
+//Tarea crear un módulo llamado contadorModule
+// declaraciones, y exportaciones
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HeroesModule,
+    ContadorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
